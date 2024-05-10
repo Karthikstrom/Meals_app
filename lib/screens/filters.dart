645dabc8fc533c8b7e646648e6eb2ebec287d05e
Karthik <karthikeyandeivamani@gmail.com>
2024-05-10 19:16:10 +0200

    After adding Pop smoke in filters page

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screens/tabs.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
@@ -29,85 +36,97 @@ class _FiltersScreenState extends State<FiltersScreen> {
           );
         }
       }),
-      body: Column(
-        children: [
-          SwitchListTile(
-            value: _glutenFreeFilterSet,
-            onChanged: (isChecked) {
-              setState(() {
-                _glutenFreeFilterSet = isChecked;
-              });
-            },
-            title: const Text(
-              'Gluten-free',
-              style:
-                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (bool didPop) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+            Filter.lactoseFree: _lactoseFreeFilterSet,
+            Filter.vegetarian: _vegetarianFilterSet,
+            Filter.vegan: _veganFilterSet
+          });
+        },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = isChecked;
+                });
+              },
+              title: const Text(
+                'Gluten-free',
+                style:
+                    TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+              ),
+              subtitle: Text(
+                'Only Include gluten free food',
+                style: TextStyle(color: Colors.blueGrey[100]),
+              ),
+              activeColor: Colors.blueGrey,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            subtitle: Text(
-              'Only Include gluten free food',
-              style: TextStyle(color: Colors.blueGrey[100]),
+            SwitchListTile(
+              value: _lactoseFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _lactoseFreeFilterSet = isChecked;
+                });
+              },
+              title: const Text(
+                'Lactose-free',
+                style:
+                    TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+              ),
+              subtitle: Text(
+                'Only Include lactose free food',
+                style: TextStyle(color: Colors.blueGrey[100]),
+              ),
+              activeColor: Colors.blueGrey,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            activeColor: Colors.blueGrey,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-          SwitchListTile(
-            value: _lactoseFreeFilterSet,
-            onChanged: (isChecked) {
-              setState(() {
-                _lactoseFreeFilterSet = isChecked;
-              });
-            },
-            title: const Text(
-              'Lactose-free',
-              style:
-                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+            SwitchListTile(
+              value: _vegetarianFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _vegetarianFilterSet = isChecked;
+                });
+              },
+              title: const Text(
+                'Vegetarian',
+                style:
+                    TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+              ),
+              subtitle: Text(
+                'Only Include vegetarian food',
+                style: TextStyle(color: Colors.blueGrey[100]),
+              ),
+              activeColor: Colors.blueGrey,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
             ),
-            subtitle: Text(
-              'Only Include lactose free food',
-              style: TextStyle(color: Colors.blueGrey[100]),
-            ),
-            activeColor: Colors.blueGrey,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-          SwitchListTile(
-            value: _vegetarianFilterSet,
-            onChanged: (isChecked) {
-              setState(() {
-                _vegetarianFilterSet = isChecked;
-              });
-            },
-            title: const Text(
-              'Vegetarian',
-              style:
-                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
-            ),
-            subtitle: Text(
-              'Only Include vegetarian food',
-              style: TextStyle(color: Colors.blueGrey[100]),
-            ),
-            activeColor: Colors.blueGrey,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          ),
-          SwitchListTile(
-            value: _veganFilterSet,
-            onChanged: (isChecked) {
-              setState(() {
-                _veganFilterSet = isChecked;
-              });
-            },
-            title: const Text(
-              'Vegan',
-              style:
-                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
-            ),
-            subtitle: Text(
-              'Only Include vegan food',
-              style: TextStyle(color: Colors.blueGrey[100]),
-            ),
-            activeColor: Colors.blueGrey,
-            contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          )
-        ],
+            SwitchListTile(
+              value: _veganFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _veganFilterSet = isChecked;
+                });
+              },
+              title: const Text(
+                'Vegan',
+                style:
+                    TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+              ),
+              subtitle: Text(
+                'Only Include vegan food',
+                style: TextStyle(color: Colors.blueGrey[100]),
+              ),
+              activeColor: Colors.blueGrey,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            )
+          ],
+        ),
       ),
     );
   }
